a28c470a4e20e4d27f2e89890468d2e12c51a763
BetterAutoFramework <luow78240@gmail.com>
2026-05-11 22:28:04 +0800

    chore: remove tools/window-pixel-studio (moved to own repo)
    Moved to https://github.com/BetterAutoFramework/window-pixel-studio

--- tools/window-pixel-studio/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { ImageWorkbench } from "./components/ImageWorkbench";
-
-export default function App() {
-  return (
-    <div className="flex h-screen min-h-0 flex-col bg-background text-foreground">
-      <ImageWorkbench />
-    </div>
-  );
-}
